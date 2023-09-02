88ee04c870a479d10be1cfbe5e99fafda6cb3b92
Thomas Elbo <telbo@deloitte.dk>
2023-09-02 10:46:51 +0200

    Added Calculator project

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}

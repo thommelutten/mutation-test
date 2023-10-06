78ab8c5e2459aae2b81cb48d731f6e6d7c610f07
Thommelutten <thomas.m.elbo@gmail.com>
2023-10-06 20:37:01 +0200

    Added Lab 1 and dotnet tool

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -2,14 +2,9 @@
 {
     public class Calculator
     {
-        public int Add(int a, int b)
+        public int Product(int a, int b)
         {
-            return a + b;
-        }
-
-        public double Divide(int a, int b)
-        {
-            return a / b;
+            return a * b;
         }
     }
 }

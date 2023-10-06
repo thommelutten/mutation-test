d6294ad4985b1245eb9c9719fa1e41b078b19a56
Thommelutten <thomas.m.elbo@gmail.com>
2023-10-06 23:11:14 +0200

    Added Lab 2 String Literals

---
+++ lab-2/HouseBuilder/HouseBuilder.Test/HouseBuilderTests.cs
@@ -0,0 +1,39 @@
+using HouseBuilder.Enums;
+using NUnit.Framework;
+
+namespace HouseBuilder.Test
+{
+    [TestFixture]
+    public class HouseBuilderTests
+    {
+        [Test]
+        public void HouseBuilder_BuildHouseWithWalls_Success()
+        {
+            House house = House.CreateBuilder()
+                .AddWall(Material.Brick)
+                .AddWall(Material.Brick)
+                .AddWall(Material.Stone)
+                .AddWall(Material.Concrete)
+                .Build();
+
+            Assert.That(house, Is.Not.Null);
+            Assert.That(house.Walls, Has.Count.EqualTo(4));
+            Assert.That(house.Walls, Does.Contain(Material.Brick));
+            Assert.That(house.Walls, Does.Contain(Material.Concrete));
+            Assert.That(house.Walls, Does.Contain(Material.Stone));
+        }
+
+        [Test]
+        public void HouseBuilder_BuildHouseWithInsufficientWalls_ThrowsException()
+        {
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+            {
+                House house = House.CreateBuilder()
+                    .AddWall(Material.Brick)
+                    .Build();
+            });
+
+            Assert.That(exception.Message, Is.EqualTo("A house must have at least 4 walls."));
+        }
+    }
+}
---
+++ lab-2/HouseBuilder/HouseBuilder/House.cs
@@ -0,0 +1,20 @@
+﻿using HouseBuilder.Enums;
+
+namespace HouseBuilder
+{
+    public class House
+    {
+        public List<Material> Walls { get; private set; }
+
+        internal House()
+        {
+            Walls = new List<Material>();
+        }
+
+        public static HouseBuilder CreateBuilder()
+        {
+            return new HouseBuilder();
+        }
+    }
+
+}
